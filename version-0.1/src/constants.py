6e93848b5ad44715146b0b1545b13bf8354c28df
weston.s.d <weston.s.d@gmail.com>
2013-05-05 23:46:10 +0000

    Version 0.1 Result of work between myself and Nicholas Seymour at Narrabri April 2013

---
+++ version-0.1/src/constants.py
@@ -0,0 +1,12 @@
+# None radio source catalog area
+# deg ^ 2
+nr_area=1.625319
+
+# Number of radio sources in radio catalogue
+
+nrs=856
+
+# Q probability that real counterpart is above
+# the detection limit
+
+Q=0.8
---
+++ version-0.1/src/select_count.py
@@ -0,0 +1,68 @@
+# randomcats.py
+# This program makes a random catalogue based on the FUSION catalogue
+
+import sys
+import math
+import array
+import random
+#import _mysql
+import MySQLdb
+import numpy
+import matplotlib.pyplot as plt
+import astropysics as astro
+import re
+
+from astropysics.constants import c,G
+
+
+#===================================================================================================
+#
+
+db=MySQLdb.connect(host="localhost",user="atlas",passwd="atlas")
+
+# Lets run a querry, find the number of records
+
+db.query("select t1.cid,t1.RA_Deg ,t1.Dec_Deg from elais_s1.coords as t1 limit 0,5;")
+
+r=db.use_result()
+
+# fetch results, returning char we need float !
+
+rows=r.fetch_row(maxrows=0)
+
+# Close connection to the database
+
+db.close()
+
+# open a new catalogue file
+
+#db.query("insert into ecdfs.cdfs_randomcat(id,ra,declination) values ('%s','%f','%f');" % (id_str,fake_ra,fake_dec))
+
+s_count=[]
+
+for row in rows:
+    print "Row : ",row
+    id=row[0]
+    ra=float(row[1])
+    dec=float(row[2])
+    db=MySQLdb.connect(host="localhost",user="atlas",passwd="atlas")
+    db.query("select count(index_spitzer) from swire_es1.es1 as t2 where pow((%f-t2.RA_SPITZER)*cos(%f),2)+ pow(%f-t2.DEC_SPITZER,2) <= pow(30/3600,2);" % (ra,dec,dec))
+    result=db.use_result()
+    count=result.fetch_row(maxrows=1)
+    print "Result: ",count[1]
+#    sc=re.findall(r'[0-9]+',count[0])
+    s_count.append(count)
+    db.close()
+	  
+
+# where pow((t1.RA_Deg-t2.RA_SPITZER)*cos(t1.Dec_Deg),2)+
+#      pow(t1.Dec_Deg-t2.DEC_SPITZER,2) <= pow(2/3600,2)
+ 
+# Close connection to the database
+
+#s_count_mean=numpy.mean(s_count)
+print "Mean Source Count with in radius: ",s_count
+
+db.close()
+         
+
