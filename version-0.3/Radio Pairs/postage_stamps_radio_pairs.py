c8cf8bd66950ebb7ce39cac2b0e720229aa13892
weston.s.d <weston.s.d@gmail.com>
2014-02-16 20:51:26 +0000

    New scripts for radio pairs.

---
+++ version-0.3/Radio Pairs/postage_stamps_radio_pairs.py
@@ -0,0 +1,126 @@
+#===========================================================================
+#
+# postage_stamp_radio_pairs.py
+#
+# Python script to query mysql databasea and create the postage
+# stamp images of the radio contours overlayed onto the spitzer image.
+#
+#===========================================================================
+#
+# S. Weston
+# AUT University
+# Feb 2014
+#===========================================================================
+
+import array
+import _mysql
+import numpy
+import math
+import sys
+import os
+
+
+radio_image_fits='d:\\elais\\atlas_elaiss1_map.fits'
+
+nonradio_image_fits='d:\\elais\\elais_s1_factor2.fits'
+
+
+print "Starting Postage Stamps"
+
+# Connect to the local database with the atlas uid
+
+db=_mysql.connect(host="localhost",user="atlas",passwd="atlas")
+
+# Lets run a querry
+# This gets a list of the possible radio pairs  
+
+db.query("select cid1, \
+	        (select ra from atlas_dr3.elais_coords where id=cid1) ra1,  \
+                (select decl from atlas_dr3.elais_coords where id=cid1) dec1, \
+	   cid2, \
+                (select ra from atlas_dr3.elais_coords where id=cid2) ra2, \
+	        (select decl from atlas_dr3.elais_coords  where id=cid2) dec2 \
+from atlas_dr3.elais_radio_pairs \
+where flag='rd';")
+
+# store_result() returns the entire result set to the client immediately.
+# The other is to use use_result(), which keeps the result set in the server
+#and sends it row-by-row when you fetch.
+
+#r=db.store_result()
+# ...or...
+r=db.use_result()
+
+# fetch results, returning char we need float !
+
+rows=r.fetch_row(maxrows=1000)
+
+# Close connection to the database
+db.close()
+
+#print rows
+
+# Connect to the local database with the atlas uid
+
+db=_mysql.connect(host="localhost",user="atlas",passwd="atlas")
+
+for row in rows:
+    cid1=row[0]
+    ra_radio1=row[1]
+    dec_radio1=row[2]
+    cid2=row[3]
+    ra_radio2=row[4]
+    dec_radio2=row[5]
+    print "Radio Pair :",cid1,ra_radio1,dec_radio1,cid2,ra_radio2,dec_radio2
+    f_ra_radio1=float(ra_radio1)
+    f_dec_radio1=float(dec_radio1)
+    f_ra_radio2=float(ra_radio2)
+    f_dec_radio2=float(dec_radio2)
+
+            
+#   Create a DS9 region file for this radio source
+
+
+    region_file_name=cid1+'_'+ra_radio1+'_'+dec_radio1+'.reg'
+    f=open(region_file_name,'w')
+    f.write('global color=blue font="helvetica 10 normal "\n')
+    # put a cross for the radio source
+    f.write('global color=red\n')
+    f.write('fk5;circle( '+ra_radio1+' , '+dec_radio1+' ,1") # point=cross text={'+cid1+'}\n')
+    f.write('fk5;circle( '+ra_radio2+' , '+dec_radio2+' ,1") # point=cross text={'+cid2+'}\n')
+
+
+    # Close the region file
+    f.close()
+
+
+    # Now create DS9 commands and execute
+
+    contour_file_name=cid1+'_'+ra_radio1+'_'+dec_radio1+'.con'
+    postage_stamp_filename1='D:\\elais\\radio_pairs\\atlas_'+cid1+'_'+cid2+'.jpeg'
+    
+    cmd1='ds9 -zscale -invert '+radio_image_fits+' -crop '+ra_radio1+' '+dec_radio1+ \
+         ' 150 150 wcs fk5 arcsec -contour open -contour loadlevels contour_ds9.lev -contour yes ' + \
+         ' -regions '+region_file_name+ ' -colorbar no ' +\
+         '-contour save '+contour_file_name+' -contour close -zoom to fit ' +\
+         '-saveimage '+postage_stamp_filename1+' 100 '
+#    print cmd1
+ 
+    postage_stamp_filename='D:\\elais\\radio_pairs\\swire_'+cid1+'_'+cid2+'_'+ra_radio1+'_'+dec_radio1+'.jpeg'
+    cmd2='ds9 -zscale -invert '+ nonradio_image_fits+' -crop '+ra_radio1+' '+dec_radio1+ \
+         ' 150 150 wcs fk5 arcsec -contour open -contour load '+contour_file_name+ \
+         ' -regions '+region_file_name+ ' -colorbar no ' +\
+         ' -contour close -zoom to fit -saveimage '+postage_stamp_filename+' 100 '
+#    print cmd2
+
+    os.system(cmd1)
+    os.system(cmd2)
+
+
+# End of do block
+
+# Close connection to the database
+db.close()
+
+print "End"
+
--- version-0.3/Radio Pairs/radio_pairs_coords.sql
+++ version-0.3/Radio Pairs/radio_pairs_coords.sql
@@ -1,7 +1,9 @@
 
-drop table atlas_dr3.elais_radio_pairs;
+set field=cdfs;
 
-CREATE TABLE atlas_dr3.elais_radio_pairs (
+drop table atlas_dr3.@field_radio_pairs;
+
+CREATE TABLE atlas_dr3.@field_radio_pairs (
   `id` int unsigned not null auto_increment,
   `cid1` varchar(8) DEFAULT NULL,
   `cid2` varchar(8) DEFAULT NULL,
@@ -14,13 +16,13 @@ CREATE TABLE atlas_dr3.elais_radio_pairs (
   KEY `idx_cid2` (`cid2`)
 ) 
 
-truncate table atlas_dr3.elais_radio_pairs;
+truncate table atlas_dr3.@field_radio_pairs;
 
 # Find pairs based on ang_sep nearest neighbour.
-insert into atlas_dr3.elais_radio_pairs(cid1,cid2,ang_sep_arcsec)
+insert into atlas_dr3.@field_radio_pairs(cid1,cid2,ang_sep_arcsec)
 select t1.id, t2.id,
        format(sqrt(pow((t1.RA-t2.RA)*cos(t1.Decl),2)+pow(t1.Decl-t2.Decl,2))*3600,6)
-from atlas_dr3.elais_coords as t1, atlas_dr3.elais_coords as t2
+from atlas_dr3.@field_coords as t1, atlas_dr3.@field_coords as t2
 where pow((t1.RA-t2.RA)*cos(t1.Decl),2)+
       pow(t1.Decl-t2.Decl,2) <= pow(100/3600,2)
 and t1.id!=t2.id
@@ -29,17 +31,17 @@ limit 0,20000;
 # now get the fluxs
 # sp-peak flux
 set sql_safe_updates=0;
-update atlas_dr3.elais_radio_pairs as t1
-   inner join atlas_dr3.elais_radio_properties as t2
+update atlas_dr3.@field_radio_pairs as t1
+   inner join atlas_dr3.@field_radio_properties as t2
    on t1.cid1=t2.id
 set t1.flux1=t2.sp;
 
-update atlas_dr3.elais_radio_pairs as t1
-   inner join atlas_dr3.elais_radio_properties as t2
+update atlas_dr3.@field_radio_pairs as t1
+   inner join atlas_dr3.@field_radio_properties as t2
    on t1.cid2=t2.id
 set t1.flux2=t2.sp;
 
-update atlas_dr3.elais_radio_pairs
+update atlas_dr3.@field_radio_pairs
 set flag='rd'
 where flux1/flux2 > 1.0
 and flux1/flux2 < 2.1
@@ -48,12 +50,12 @@ and ang_sep_arcsec/sqrt(flux1+flux2) < 10.0)
 
 # select id's and coord's for postage stamps.
 select cid1, 
-	        (select ra from atlas_dr3.elais_coords where id=cid1) ra1,  
-                (select decl from atlas_dr3.elais_coords where id=cid1) dec1, 
+	        (select ra from atlas_dr3.@field_coords where id=cid1) ra1,  
+                (select decl from atlas_dr3.@field_coords where id=cid1) dec1, 
 	   cid2, 
-                (select ra from atlas_dr3.elais_coords where id=cid2) ra2,
-	        (select decl from atlas_dr3.elais_coords  where id=cid2) dec2 
-from atlas_dr3.elais_radio_pairs
+                (select ra from atlas_dr3.@field_coords where id=cid2) ra2,
+	        (select decl from atlas_dr3.@field_coords  where id=cid2) dec2 
+from atlas_dr3.@field_radio_pairs
 where flag='rd';
 		  
 
