b14caf143c90a161078c5cb59c91d35cd7cb950e
weston.s.d <weston.s.d@gmail.com>
2014-06-16 08:20:39 +0000

    Getting plots for rel & lr to be stepped histograms. Success

--- version-0.5/plot_lr_vs_rel.py
+++ version-0.5/plot_lr_vs_rel.py
@@ -77,15 +77,26 @@ def plot_lr_rel():
 
 #    (hist,bins)=numpy.histogram(f_rows,bins=60,range=[-1.0,5.0])
     (hist,bins)=numpy.histogram(REL,bins=10,range=[0.0,1.0])
+    print bins,'\n'
+    print hist,'\n'
     width = 1.0*(bins[1]-bins[0])
-    center = (bins[:-1]+bins[1:])/2
+#    center = (bins[:-1]+bins[1:])/2
+    center = 0.5*(bins[1:]+bins[:-1])
+    plot_title='ATLAS ' +field+ ' Histogram of the Reliability values'
+    plt.title(plot_title)
     plt.yscale('log')
-    plt.xlim((-0.1,1.1))
+    plt.grid(True)
+#    plt.xlim((-0.1,1.1))
     plt.ylabel('N(counterparts)')
     plt.xlabel('Reliability')
 #   edgecolor, linestyle, linewidth
-    plt.bar(center, hist, align = 'center',fill=False,edgecolor='0.0', width = width,linewidth=1)
-#    plt.plot(center, hist, 'k--', linewidth=1.5)
+#    plt.bar(center, hist, align = 'center',fill=False,edgecolor='0.0', width = width,linewidth=1)
+#    plt.plot(center, hist, 'k-')
+    common_params = dict(bins=10,
+                         range=(0,1))
+    common_params['histtype'] = 'step'
+    plt.hist(REL,**common_params)
+#    pylab.hist(hist, bins=bins, normed=1,histtype='step')
     plot_fname='atlas_' +field+ '_N_vs_rel.ps'
     fname=output_dir + plot_fname
     plt.savefig(fname,orientation='landscape')
@@ -93,15 +104,23 @@ def plot_lr_rel():
 	
 # Bin LR and plot
 
-    (hist,bins)=numpy.histogram(LOG10_LR,bins=10,range=[0.0,10.0])
+    (hist,bins)=numpy.histogram(LR,bins=100,range=[0.0,1000.0])
     width = 1.0*(bins[1]-bins[0])
-    center = (bins[:-1]+bins[1:])/2
+    center = 0.5*(bins[:-1]+bins[1:])
+    print bins,'\n'
+    print hist,'\n'
+    plot_title='ATLAS ' +field+ ' Histogram of the Likelihood Ratio values'
+    plt.title(plot_title)
     plt.yscale('log')
-#    plt.xscale('log')
+    plt.xscale('log')
     plt.ylabel('N(counterparts)')
     plt.xlabel('Likelihood Ratio')
 #   edgecolor, linestyle, linewidth
-    plt.bar(center, hist, align = 'center',fill=False,edgecolor='0.0', width = width,linewidth=1)
+#    plt.bar(center, hist, align = 'center',fill=False,edgecolor='0.0', width = width,linewidth=1)
+    common_params = dict(bins=100,
+                         range=(0,1000))
+    common_params['histtype'] = 'step'
+    plt.hist(LR,**common_params)
 #    plt.plot(center, hist, 'k--', linewidth=1.5)
     plot_fname='atlas_' +field+ '_N_vs_lr.ps'
     fname=output_dir + plot_fname
