a17f9bc1289aca08b5a75895cef084dc6e65e570
weston.s.d <weston.s.d@gmail.com>
2014-11-30 22:51:53 +0000

    New ATLAS DR Catalogue - some table/column changes

--- version-0.5-swire/Database Schema/atlas_dr3_populate_normalised_tables_cdfs.sql
+++ version-0.5-swire/Database Schema/atlas_dr3_populate_normalised_tables_cdfs.sql
@@ -2,28 +2,28 @@ truncate table atlas_dr3.cdfs_coords;
 
 insert into atlas_dr3.cdfs_coords (id,ra,ra_err,decl,decl_err)
 select id,ra,ra_err,decl,decl_err
-from atlas_dr3.cdfs_cmpcat_17june2014;
+from atlas_dr3.cdfs_cmpcat_24nov2014;
 
 truncate table atlas_dr3.cdfs_name;
 
-insert into atlas_dr3.cdfs_name (id,survey,name)
-select id,name1,name2
-from atlas_dr3.cdfs_cmpcat_17june2014;
+insert into atlas_dr3.cdfs_name (id,name)
+select id,name
+from atlas_dr3.cdfs_cmpcat_24nov2014;
 
 truncate table atlas_dr3.cdfs_deconv;
 
 insert into atlas_dr3.cdfs_deconv (id,deconv,deconv_err)
 select id,deconv,deconv_err
-from atlas_dr3.cdfs_cmpcat_17june2014;
+from atlas_dr3.cdfs_cmpcat_24nov2014;
 
 truncate table atlas_dr3.cdfs_sindex;
 
 insert into atlas_dr3.cdfs_sindex (id,sindex,sindex_err)
-select id,sindex,index_err
-from atlas_dr3.cdfs_cmpcat_17june2014;
+select id,sindex,sindex_err
+from atlas_dr3.cdfs_cmpcat_24nov2014;
 
 truncate table atlas_dr3.cdfs_radio_properties;
 
-insert into atlas_dr3.cdfs_radio_properties (id,snr,rms,bws,sp,sp_err,sint,sint_err,obs_freq)
-select id,snr,rms,bws,sp,sp_err,sint,sint_err,obs_freq
-from atlas_dr3.cdfs_cmpcat_17june2014;
+insert into atlas_dr3.cdfs_radio_properties (id,rms,bws,sp,sp_err,sint,sint_err,obs_freq)
+select id,rms,bws,sp,sp_err,sint,sint_err,obs_freq
+from atlas_dr3.cdfs_cmpcat_24nov2014;
